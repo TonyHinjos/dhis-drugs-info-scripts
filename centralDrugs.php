72271770d6dd6bafd35af14bbb3240dbe2052c92
TonyHinjos <awanjohim@gmail.com>
2015-06-15 10:54:38 +0300

    fetching data by passing ids from database

--- centralDrugs.php
+++ centralDrugs.php
@@ -4,18 +4,17 @@ include('includes/connect.php');
   $period=$_POST['period'];
   $category=$_POST['drug_category_id'];
   $value=$_POST['drug_value'];
-  $countyId=$_POST['county_id'];
  
 if(isset($_POST['submit']))
   
 {
-  if(!isset($id) ||!isset($period) || !isset($category)||!isset($value)|| !isset($countyId))
+  if(!isset($id) ||!isset($period) || !isset($category)||!isset($value))
   {
     echo "something is wrong";
   }
   else
   {
-    $sql="INSERT into county_level_drugs(drug_id,period,drug_category_id,drug_value,county_id)values('$id','$period','$category','$value','$countyId')"; 
+    $sql="INSERT into central_level_drugs(drug_id,period,drug_category_id,drug_value)values('$id','$period','$category','$value')"; 
     $qury=mysql_query($sql);
     if(!$qury)
     { 
